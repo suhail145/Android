c843dad6b51fa0122082a1a65bbbba912dd1996b
suhail145 <suhail145@gmail.com>
2013-09-16 18:03:24 +0530

    check colors added

---
+++ CashBook/src/com/DroidApps/cashbook/CheckColors.java
@@ -0,0 +1,36 @@
+package com.DroidApps.cashbook;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckColors extends Activity {
+
+	ArrayList<Color> colors;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.check_color);
+		colors = new ArrayList<Color>();
+		
+	}
+
+	private class Color{
+		String name;
+		String value;
+		
+		Color(String name, String value){
+			this.name=name;
+			this.value=value;
+		}
+		
+		public String getColor(){
+			return this.value;
+		}
+	}
+	
+}
--- CashBook/src/com/DroidApps/cashbook/TransactionDisplay.java
+++ CashBook/src/com/DroidApps/cashbook/TransactionDisplay.java
@@ -3,9 +3,11 @@ package com.DroidApps.cashbook;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TransactionDisplay extends Activity {
@@ -13,16 +15,22 @@ public class TransactionDisplay extends Activity {
 	public static final String TAG = TransactionDisplay.class.getSimpleName();
 	DbUpdater db;
 	ListView lvTransDisp;
+	TextView date;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_transaction_display);
 		db = new DbUpdater(this);
+		date = new TextView(this);
+		date.setBackgroundColor(getResources().getColor(R.color.Azure));
+		date.setText("Date");
 		ArrayList<Transaction> array = db.getAllTransactions();
 		if (array != null) {
 			lvTransDisp = (ListView) findViewById(R.id.lvTransDisplay);
 			TransAdapter ad = new TransAdapter(this, array);
+			lvTransDisp.setFastScrollEnabled(true);
+			lvTransDisp.addHeaderView(date);
 			lvTransDisp.setAdapter(ad);
 		} else {
 			Toast.makeText(this, "No transactions to display! Add some",
