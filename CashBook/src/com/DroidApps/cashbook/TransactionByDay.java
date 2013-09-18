8e69344e6bed5d6d4262ea2510270e38ba46ef7d
suhail145 <suhail145@gmail.com>
2013-09-19 00:23:40 +0530

    Fixed transaction by day

--- CashBook/src/com/DroidApps/cashbook/TransactionByDay.java
+++ CashBook/src/com/DroidApps/cashbook/TransactionByDay.java
@@ -8,30 +8,64 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
 
 public class TransactionByDay extends Activity {
-	
-	ListView lvTransByDay,lvTrans;
+
+	ExpandableListView lvTransByDay;
 	DbUpdater db;
-	HashMap<String,ArrayList<Transaction>> dayTrans;
-	
+	ArrayList<String> TransDates;
+	HashMap<String, ArrayList<Transaction>> dayTrans;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_transaction_by_day);
 		db = new DbUpdater(this);
-		dayTrans = new HashMap<String,ArrayList<Transaction>>();
-		
-		Iterator<String> dates = db.getTransactionDates().iterator();
-		while(dates.hasNext()){
-			String date=dates.next();
+		lvTransByDay = (ExpandableListView) findViewById(R.id.lvTransByDay);
+		dayTrans = new HashMap<String, ArrayList<Transaction>>();
+		TransDates = db.getTransactionDates();
+
+		Iterator<String> dates = TransDates.iterator();
+		while (dates.hasNext()) {
+			String date = dates.next();
 			ArrayList<Transaction> trans = db.getDayTransactions(date);
-			dayTrans.put(date,trans);
-			
+			dayTrans.put(date, trans);
+
 		}
-		
+		TransByDayAdapter adapter = new TransByDayAdapter(this, dayTrans,
+				TransDates);
+		lvTransByDay.setAdapter(adapter);
+
+		lvTransByDay.setOnGroupClickListener(new OnGroupClickListener() {
+
+			@Override
+			public boolean onGroupClick(ExpandableListView parent, View v,
+					int groupPosition, long id) {
+
+				return false;
+			}
+		});
+		lvTransByDay.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+			@Override
+			public void onGroupExpand(int groupPosition) {
+
+			}
+
+		});
+
+		lvTransByDay.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+			@Override
+			public void onGroupCollapse(int groupPosition) {
+
+			}
+		});
+
 	}
 
 	@Override
